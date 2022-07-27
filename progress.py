ce95cc2738c4969b944c0e5004a10c83c71a9176
ClientFromHell <s.zheludev@gmail.com>
2022-07-27 09:30:27 +0300

    Finally Ended Hard Task + Added Flake8 watchers "27.08.2022"

--- progress.py
+++ progress.py
@@ -1,4 +1,6 @@
-# n = int(input())
+n = int(input())
+
+values_list, results = [], []
 
 stack = {
     'global': {
@@ -6,33 +8,31 @@ stack = {
         'parent': None
     }
 }
-vars = []
-results = []
-TEST_COMMANDS = ['add global a',
-                 'create foo global',
-                 'add foo b',
-                 'get foo a',
-                 'get foo c',
-                 'create bar foo',
-                 'add bar a',
-                 'get bar a',
-                 'get bar b'
-                 ]
 
-stack = {
-    'global': {
-        'variable': [],
-        'parent': None
-    }
-}
 
 def add(namesp: str, var: int):
+
+    """
+    Adds elements into known namespaces and all variabls into values_list
+    :param namesp:
+    :param var:
+    :return:
+    """
+
     global stack
     stack[namesp]['variable'].append(var)
-    vars.append(var)
+    values_list.append(var)
 
 
 def create(namesp: str, parent: str):
+
+    """
+    Creates new namespace
+    :param namesp:
+    :param parent:
+    :return:
+    """
+
     global stack
     if namesp not in stack:
         stack[namesp] = {
@@ -42,16 +42,24 @@ def create(namesp: str, parent: str):
 
 
 def get_var(namesp: str, var: int):
+
+    """
+    Returns the namespace of incomming var or None if namespace\var doesn't exsist
+    :param namesp:
+    :param var:
+    :return:
+    """
+
     global stack
-    if var not in vars:
+    if var not in values_list or namesp not in stack:
         return None
     else:
         return namesp if var in stack[namesp]['variable'] \
             else get_var(stack[namesp]['parent'], var)
 
 
-for i in TEST_COMMANDS:
-    command, namesp, var_par = i.split() # input().split()
+for i in range(n):
+    command, namesp, var_par = input().split()
     if command == 'add':
         add(namesp, var_par)
     elif command == 'create':
@@ -59,6 +67,5 @@ for i in TEST_COMMANDS:
     else:
         res = get_var(namesp, var_par)
         results.append(res)
-
 
 print(*results, sep='\n')
