f32eb084aaedc443627779585db29989df7daa67
ClientFromHell <s.zheludev@gmail.com>
2022-07-28 13:54:24 +0300

    MoneyBox & Buffer tasks "28.08.2022"

--- MoneyBox_and_Buffer_tasks.py
+++ MoneyBox_and_Buffer_tasks.py
@@ -1,29 +1,29 @@
 # ========================== MoneyBox task ==========================
 #
-# class MoneyBox:
-#
-#     def __init__(self, capacity):
-#         # конструктор с аргументом – вместимость копилки
-#         self.capacity = capacity
-#         self.v = 0
-#
-#     def can_add(self, v):
-#         # True, если можно добавить v монет, False иначе
-#         self.v = v
-#         if self.v <= self.capacity:
-#             return True
-#         else:
-#             return False
-#
-#
-#     def add(self, v):
-#         # положить v монет в копилку
-#         self.v = v
-#         if self.can_add(self.v) and self.capacity >= 0 and self.capacity >= self.v:
-#             self.capacity = self.capacity - self.v
-#         else:
-#             return False
-#
+class MoneyBox:
+
+    def __init__(self, capacity):
+        # конструктор с аргументом – вместимость копилки
+        self.capacity = capacity
+        self.v = 0
+
+    def can_add(self, v):
+        # True, если можно добавить v монет, False иначе
+        self.v = v
+        if self.v <= self.capacity:
+            return True
+        else:
+            return False
+
+
+    def add(self, v):
+        # положить v монет в копилку
+        self.v = v
+        if self.can_add(self.v) and self.capacity >= 0 and self.capacity >= self.v:
+            self.capacity = self.capacity - self.v
+        else:
+            return False
+
 # ========================== Bufer task ==========================
 #
 class Buffer:
