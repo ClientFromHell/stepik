282b742b02f4a92a058b256fc590ecd5eacb81ab
ClientFromHell <s.zheludev@gmail.com>
2022-08-02 18:35:05 +0300

    GRAPH 2.07.2022

--- inheritance.py
+++ inheritance.py
@@ -1,96 +1,30 @@
-import collections, sys, pprint
-
-
-# TESTS:
-
-DICT_CLASS = {
-    'A': ['B', 'C', 'D', 'G', 'H'],
-    'B': ['C', 'E', 'G', 'H', 'K', 'L'],
-    'C': ['E', 'D', 'H', 'K', 'L'],
-    'D': ['G', 'H'],
-    'E': ['D', 'F', 'K', 'L'],
-    'F': ['K'],
-    'G': ['F'],
-    'H': ['L'],
-    'K': ['H', 'L'],
-    'L': []
-                  }
-
-CLS_CHECKER = [
-                 ['K', 'D'],
-                 ['D', 'A'],
-                 ['G', 'D'],
-                 ['H', 'A'],
-                 ['E', 'E'],
-                 ['H', 'G'],
-                 ['E', 'L'],
-                 ['D', 'L'],
-                 ['B', 'D'],
-                 ['D', 'G'],
-                 ['D', 'E'],
-                 ['A', 'F'],
-                 ['A', 'C'],
-                 ['K', 'A'],
-                 ['A', 'H'],
-                 ['K', 'D'],
-                 ['H', 'B'],
-                 ['K', 'B'],
-                 ['D', 'L'],
-                 ['G', 'G'],
-                 ['A', 'H'],
-                 ['K', 'L'],
-                 ['G', 'E'],
-                 ['B', 'A'],
-                 ['C', 'K'],
-                 ['K', 'L'],
-                 ['C', 'L'],
-                 ['G', 'C'],
-                 ['D', 'D'],
-                 ['C', 'G'],
-                 ['E', 'A'],
-                 ['F', 'K'],
-                 ['B', 'G'],
-                 ['H', 'L'],
-                 ['L', 'F'],
-                 ['H', 'G'],
-                 ['D', 'A'],
-                 ['H', 'L']
-               ]
-
-
-# DICT_CLASS = {}
-# CLS_CHECKER = []
 inherit = []
+CLS_CHECKER = []
+DICT_CLASS = {}
 elements = []
 RESULTS = []
-
-visited = []  # List to keep track of visited nodes.
-queue = []  # Initialize a queue
-res = []
+queue = []
 
 
-def bfs(visited, graph, node):
-    visited.append(node)
-    for i in graph[node]:
-        if i in graph:
-            bfs(visited, graph, i)
-        else:
-            visited.append(i)
+def bfs(queue, visited, graph):
+    while queue:
+        for node in queue:
+            visited.add(node)
+            if node not in graph:
+                queue.remove(node)
+            else:
+                queue += graph[node]
+                queue.remove(node)
     return visited
 
 
-# for i in range(int(sys.stdin.readline().strip())):
-#     inherit.append([num for num in filter(lambda num: num.isalpha(), sys.stdin.readline().strip())])
-#
-# for i in range(int(sys.stdin.readline())):
-#     CLS_CHECKER.append(sys.stdin.readline().strip().split())
+for i in range(int(sys.stdin.readline())):
+    inherit.append(input().replace(':', '').split())
 
-i_test = []
+for i in range(int(sys.stdin.readline())):
+    CLS_CHECKER.append(input().split())
 
 for i in inherit:
-    i_test.append([num for num in filter(lambda num: num.isalpha(), i)])
-
-for i in i_test:
     elements += i
     if len(i) == 1:
         DICT_CLASS[i[0]] = []
@@ -99,20 +33,19 @@ for i in i_test:
 
 elements = set(elements)
 
-
 for i in CLS_CHECKER:
-    res = []
-    visited = []
-    if i[0] not in elements or i[-1] not in elements:
-        RESULTS.append('No')
-    elif i[0] == i[-1]:
+
+    if i[0] == i[-1]:
         RESULTS.append('Yes')
+    elif i[0] not in elements or i[-1] not in elements:
+        RESULTS.append('No')
     else:
-        res = bfs(visited, DICT_CLASS, i[-1])
+        queue.append(i[-1])
+        visited = set()
+        res = bfs(queue, visited, DICT_CLASS)
         if i[0] in res:
             RESULTS.append('Yes')
         else:
             RESULTS.append('No')
 
-# print(RESULTS)
-# print(*RESULTS, sep='\n')
+print(*RESULTS, sep='\n')
