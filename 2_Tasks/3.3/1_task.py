c0cd4dddf44fbb692f211a55e6a028145c5cbb10
ClientFromHell <s.zheludev@gmail.com>
2022-08-23 16:12:20 +0300

    1st requests lib task 23.08.2022

--- 2_Tasks/3.3/1_task.py
+++ 2_Tasks/3.3/1_task.py
@@ -3,18 +3,25 @@ import requests, re
 results = []
 urls_on_first_page = []
 patternURL = r'(http*.*html)'
-# url_p = input().strip()
-# url_d = input().strip()
-url_p = 'https://stepic.org/media/attachments/lesson/24472/sample0.html'
-url_d = 'https://stepic.org/media/attachments/lesson/24472/sample1.html'
+url_p = input().strip()
+url_d = input().strip()
+
+
+def urlstatuschecker(url):
+    return True if url.status_code == 200 else False
+
 
 req = requests.get(url_p)
-if req.status_code == 200:
-    temp = re.findall(patternURL, req.text)
-    for link in urls_on_first_page:
-        req = requests.get(link)
-        if req.status_code == 200 and:
 
-print(urls_on_first_page)
+if urlstatuschecker(req):
+    urls_on_first_page = re.findall(patternURL, req.text)
 
+for link in urls_on_first_page:
+    req = requests.get(link)
+    if urlstatuschecker(req):
+        results.extend(re.findall(patternURL, req.text))
 
+if url_d in results:
+    print('Yes')
+else:
+    print('No')
