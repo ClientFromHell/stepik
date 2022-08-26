4a16d67c8f36791a1adca8f4b2c0d99b8fde2b0b
ClientFromHell <s.zheludev@gmail.com>
2022-08-26 18:22:26 +0300

    1st CSV lib task 26.08.2022

--- 2_Tasks/3.4/2_task.py
+++ 2_Tasks/3.4/2_task.py
@@ -1,12 +1,14 @@
-import json, pprint
+import json
 
 
-jsn = [
-    {'name': 'A', 'parents': []},
-    {'name': 'B', 'parents': ['A', 'C']},
-    {'name': 'C', 'parents': ['A']},
-    {'name': 'D', 'parents': ['B']}
-    ]
+jsn = input()
+jsn = json.loads(jsn)
+    # [
+    # {'name': 'A', 'parents': []},
+    # {'name': 'B', 'parents': ['A', 'C']},
+    # {'name': 'C', 'parents': ['A']},
+    # {'name': 'D', 'parents': ['B']}
+    # ]
 
 cls_dict = {}
 class_counts = {}
@@ -25,15 +27,24 @@ def graph(way, point):
 
 for value in jsn:
     cls_dict[value['name']] = value['parents']
-    class_counts[value['name']] = 1
+    class_counts[value['name']] = 0
     available_classes.append(value['name'])
 
 for point in available_classes:
     visited_points = []
     results = graph(cls_dict, point)
-    print(set(visited_points))
+    for i in set(visited_points):
+        class_counts[i] += 1
+
+available_classes = sorted(available_classes)
+for elem in available_classes:
+    print(f'{elem} : {class_counts[elem]}')
+
+
+
+
 # print(available_classes)
-print(class_counts)
+# print(class_counts)
 # print(cls_dict)
 
 
